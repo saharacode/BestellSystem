4276189a913509327f4423ae254b166f5b4e2f90
saharacode <johanndallmann@gmx.de>
2023-04-28 18:36:56 +0200

    properties, constructors, methods, get/set for ShopService class created

--- src/main/java/de/neuefische/models/ShopService.java
+++ src/main/java/de/neuefische/models/ShopService.java
@@ -1,20 +1,67 @@
 package de.neuefische.models;
 
+import java.util.Objects;
+
 public class ShopService {
     // properties
     private ProductRepo productRepo;
     private OrderRepo orderRepo;
 
 
+
+
     // constructors
+    public ShopService(){}
+    public ShopService(ProductRepo productRepo, OrderRepo orderRepo) {
+        this.productRepo = productRepo;
+        this.orderRepo = orderRepo;
+    }
+
 
 
 
     // methods
+    @Override
+    public String toString() {
+        return "ShopService{" +
+                "productRepo=" + productRepo +
+                ", orderRepo=" + orderRepo +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ShopService that = (ShopService) o;
+        return Objects.equals(productRepo, that.productRepo) && Objects.equals(orderRepo, that.orderRepo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productRepo, orderRepo);
+    }
+
+
+
+
 
 
 
     // get/set
+    public ProductRepo getProductRepo() {
+        return productRepo;
+    }
 
+    public void setProductRepo(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
 
+    public OrderRepo getOrderRepo() {
+        return orderRepo;
+    }
+
+    public void setOrderRepo(OrderRepo orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 }
