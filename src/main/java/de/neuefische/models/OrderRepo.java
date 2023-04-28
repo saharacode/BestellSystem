707661d38c754d32d8722a99f568517f418618f1
saharacode <johanndallmann@gmx.de>
2023-04-28 12:12:57 +0200

    properties, constructors, methods, get/set added for Repos

--- src/main/java/de/neuefische/models/OrderRepo.java
+++ src/main/java/de/neuefische/models/OrderRepo.java
@@ -1,19 +1,52 @@
 package de.neuefische.models;
 
+import java.util.Map;
+import java.util.Objects;
+
 public class OrderRepo {
     // properties
+    private Map<String,Order> repoOrders;
 
 
 
     // constructors
+    public OrderRepo(){}
+    public OrderRepo(Map<String, Order> repoOrders) {
+        this.repoOrders = repoOrders;
+    }
 
 
 
     // methods
+    @Override
+    public String toString() {
+        return "OrderRepo{" +
+                "repoOrders=" + repoOrders +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderRepo orderRepo = (OrderRepo) o;
+        return Objects.equals(repoOrders, orderRepo.repoOrders);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(repoOrders);
+    }
+
 
 
 
     // get/set
+    public Map<String, Order> getRepoOrders() {
+        return repoOrders;
+    }
 
-
+    public void setRepoOrders(Map<String, Order> repoOrders) {
+        this.repoOrders = repoOrders;
+    }
 }
--- src/main/java/de/neuefische/models/ProductRepo.java
+++ src/main/java/de/neuefische/models/ProductRepo.java
@@ -1,19 +1,52 @@
 package de.neuefische.models;
 
+import java.util.Map;
+import java.util.Objects;
+
 public class ProductRepo {
     // properties
+    private Map<String,Product> repoProducts;
 
 
 
     // constructors
+    public ProductRepo(){}
+    public ProductRepo(Map<String, Product> repoProducts) {
+        this.repoProducts = repoProducts;
+    }
 
 
 
     // methods
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "repoProducts=" + repoProducts +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductRepo that = (ProductRepo) o;
+        return Objects.equals(repoProducts, that.repoProducts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(repoProducts);
+    }
+
 
 
 
     // get/set
+    public Map<String, Product> getRepoProducts() {
+        return repoProducts;
+    }
 
-
+    public void setRepoProducts(Map<String, Product> repoProducts) {
+        this.repoProducts = repoProducts;
+    }
 }
